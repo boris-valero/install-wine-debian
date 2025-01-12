e003f66f98848aad09abcbae98f3d06fc00f7405
boris-valero <157314735+boris-valero@users.noreply.github.com>
2025-01-12 12:57:20 +0100

    Add Scripts

---
+++ install_wine_bullseye.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Ajouter l'architecture i386
+sudo dpkg --add-architecture i386
+
+# Mettre à jour la liste des paquets
+sudo apt update
+
+# Télécharger la clé WineHQ
+wget -nc https://dl.winehq.org/wine-builds/winehq.key
+
+# Importer la clé WineHQ
+gpg -n -a --batch --import winehq.key
+gpg --no-default-keyring --keyring ./winehq.key.gpg --import winehq.key
+gpg --no-default-keyring --keyring ./winehq.key.gpg --export > /usr/share/keyrings/winehq-archive.keyring
+
+# Ajouter le dépôt WineHQ
+echo "deb [signed-by=/usr/share/keyrings/winehq-archive.keyring] https://dl.winehq.org/wine-builds/debian/ bullseye main" | sudo tee /etc/apt/sources.list.d/winehq.list
+
+# Mettre à jour la liste des paquets à nouveau
+sudo apt update
+
+# Installer WineHQ stable
+sudo apt install --install-recommends winehq-stable
